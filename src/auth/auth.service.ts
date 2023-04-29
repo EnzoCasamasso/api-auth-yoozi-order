e2c309d0f75e8347399bbc364613243bca8dddf0
Enzo <enzocasamasso@gmail.com>
2023-04-29 10:57:51 -0300

    Adicionando controller de auth e adicionando guards

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor() {
+
+    }
+    
+    login() {
+
+    }
+
+    validateUser(email, pass) {
+
+    }
+}
