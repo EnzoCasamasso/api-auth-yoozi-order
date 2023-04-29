0ae1b5e52f1f9b4ba53e292f58c9b069c8f3832f
Enzo <enzocasamasso@gmail.com>
2023-04-28 23:41:53 -0300

    Adicionando modulo de autenticação auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { ProductsModule } from './modules/products/products.module';
 import { BusinessModule } from './modules/business/business.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     UsersModule,
     ProductsModule,
-    BusinessModule
+    BusinessModule,
+    AuthModule
   ],
   controllers: [],
   providers: [],
--- src/modules/business/business.controller.ts
+++ src/modules/business/business.controller.ts
@@ -13,7 +13,6 @@ export class BusinessController {
         return businessDto;
     }
 
-
     @Get()
     async getAllBusiness() {
         return this.businessService.getAllBusiness();
