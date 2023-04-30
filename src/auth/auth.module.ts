3ced8dd2d1f34f2ca1e15fae48dc5127646b22b9
Enzo <enzocasamasso@gmail.com>
2023-04-29 22:17:25 -0300

    added jwrStrategy, login-validation, middlewares

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,27 @@
-import { Module } from '@nestjs/common';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { BusinessModule } from 'src/modules/business/business.module';
+import { LoginValidationMiddleware } from './middlewares/login-validation.middleware';
 
 @Module({
-  imports: [BusinessModule, PassportModule],
+  imports: [
+    BusinessModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '30d' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy]
+  providers: [AuthService, LocalStrategy, JwtStrategy]
 })
-export class AuthModule {}
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoginValidationMiddleware).forRoutes('login');
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,23 +2,28 @@ import { Injectable } from '@nestjs/common';
 import { BusinessService } from 'src/modules/business/business.service';
 import * as bcrypt from 'bcrypt' 
 import { UnauthorizedError } from './errors/unauthorized.error';
+import { Business } from 'src/entities/business.entity';
+import { UserPayload } from './models/UserPayload';
+import { UserToken } from './models/UserToken';
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
-    constructor(private readonly businessService: BusinessService) {
+    constructor(
+        private readonly businessService: BusinessService,
+        private readonly jwtService: JwtService
+    ) { }
 
-    }
-
-    // async login(business: Business): Promise<UserToken> {
-    //     const payload: UserPayload = {
-    //       sub: business.id,
-    //       email: business.email,
-    //       name: business.businessName,
-    //     };
+    async login(business: Business): Promise<UserToken> {
+        const payload: UserPayload = {
+          sub: business.id,
+          email: business.email,
+          name: business.businessName,
+        };
     
-    //     return {
-    //       access_token: this.jwtService.sign(payload),
-    //     };
-    //   }
+        return {
+          access_token: this.jwtService.sign(payload),
+        };
+      }
 
     async validateBusinessOrSeller(email: string, password: string) {
         const business = await this.businessService.findByEmail(email)
