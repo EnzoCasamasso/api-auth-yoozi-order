b361c4cbd50323fb50e4118af56c67ece2135f94
Enzo <enzocasamasso@gmail.com>
2023-04-30 00:45:33 -0300

    feat(AppService) operant service

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getWorks(): string {
-    return 'API works!';
+  getWorks() {
+    return {
+      name:'yoozy-order-api',
+      status: 'operant',
+      version: '1.0.0'
+    };
   }
 }
