079c81c4e3cb9376409ebe8e578550a3bf2e874c
Enzo <enzocasamasso@gmail.com>
2023-04-29 23:04:25 -0300

    Adicionando appService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { BusinessModule } from './modules/business/business.module';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { AppService } from './app.service';
 
 @Module({
   imports: [PrismaModule, BusinessModule, AuthModule],
   controllers: [AppController],
   providers: [
-  ],
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ]
 })
 export class AppModule {}
